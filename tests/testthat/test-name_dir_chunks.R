d769c2c8783248abb90bcb4926ff8d301ea5265f
Maëlle Salmon <maelle.salmon@yahoo.se>
2018-09-06 15:52:29 +0200

    add test for name_dir_chunks

---
+++ tests/testthat/test-name_dir_chunks.R
@@ -0,0 +1,12 @@
+context("test-name_dir_chunks")
+
+test_that("renaming works for a dir", {
+  fs::dir_copy(system.file("examples", package = "namer"),
+            "test")
+  name_dir_chunks("test")
+  expect_true(all(
+    extract_chunks_names(file.path("test", "example1.Rmd")) != ""))
+  expect_true(all(
+    extract_chunks_names(file.path("test", "example2.Rmd")) != ""))
+  fs::dir_delete("test")
+})
