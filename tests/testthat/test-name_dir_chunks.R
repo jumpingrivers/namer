ff3ba50a4ee4dd0c23d36a33bdf4416f92a9bf51
Maëlle Salmon <maelle.salmon@yahoo.se>
2018-09-11 14:25:08 +0200

    fix test? :pray:

--- tests/testthat/test-name_dir_chunks.R
+++ tests/testthat/test-name_dir_chunks.R
@@ -1,14 +1,18 @@
 context("test-name_dir_chunks")
 
 test_that("renaming works for a dir", {
-  fs::dir_delete("test")
+  if(fs::dir_exists("test")){
+    fs::dir_delete("test")
+  }
+
+  fs::dir_create("test")
   fs::dir_copy(system.file("examples", package = "namer"),
             "test")
-  fs::file_delete(file.path("test", "example4.Rmd"))
-  name_dir_chunks("test")
+  fs::file_delete(file.path("test", "examples", "example4.Rmd"))
+  name_dir_chunks(file.path("test", "examples"))
   expect_true(all(
-    extract_chunks_names(file.path("test", "example1.Rmd")) != ""))
+    extract_chunks_names(file.path("test", "examples", "example1.Rmd")) != ""))
   expect_true(all(
-    extract_chunks_names(file.path("test", "example2.Rmd")) != ""))
+    extract_chunks_names(file.path("test", "examples", "example2.Rmd")) != ""))
   fs::dir_delete("test")
 })
