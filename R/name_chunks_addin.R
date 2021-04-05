800cc7659dfd02ffcd52b50fb27e15d2449334ab
statnmap <sebastien@thinkr.fr>
2021-04-05 15:02:41 +0200

    Add new addins
    - Name current Rmd file
    - Name file chosen in a directory
    - Name chunks from a folder
    - issue #13
    
    - Allow to use 'prefix' in the Addin for current file in relation to
    issue #2

--- R/name_chunks_addin.R
+++ R/name_chunks_addin.R
@@ -1,3 +1,17 @@
-name_chunks_addin <- function(){
+name_chunks_file_addin <- function(){
   name_chunks(path = rstudioapi::selectFile(filter = "R Markdown Files (*.Rmd)"))
+}
+
+name_chunks_addin <- function(){
+  prefix <- rstudioapi::showPrompt(title = "Chunk names prefix",
+                                   message = "Leave empty to use filename (Default)", default = "")
+  if (prefix == "") {
+    name_chunks(path = rstudioapi::getActiveDocumentContext()$path)
+  } else {
+    name_chunks(path = rstudioapi::getActiveDocumentContext()$path, prefix = prefix)
+  }
+}
+
+name_chunks_directory_addin <- function(){
+  namer::name_dir_chunks(rstudioapi::selectDirectory())
 }
