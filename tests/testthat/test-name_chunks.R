16b692b6e6641ed919cd2b09f30f007e1bfe228f
Colin Gillespie <colin@jumpingrivers.com>
2025-01-23 09:14:19 +0000

    Add prefix and new Addins (#40)
    * Allow to choose prefix for chunk names
    
    issue #2
    
    * Add new addins
    
    - Name current Rmd file
    - Name file chosen in a directory
    - Name chunks from a folder
    - issue #13
    
    - Allow to use 'prefix' in the Addin for current file in relation to
    issue #2
    
    * Update Rproj
    
    * Rebasing
    
    ---------
    
    Co-authored-by: statnmap <sebastien@thinkr.fr>

--- tests/testthat/test-name_chunks.R
+++ tests/testthat/test-name_chunks.R
@@ -25,6 +25,18 @@ test_that("renaming works", {
   file.remove(temp_file_path)
 })
 
+test_that("renaming with prefix works", {
+  temp_file_path <- file.path(tmp, "test.Rmd")
+
+  file.copy(system.file("examples", "example1.Rmd", package = "namer"),
+            temp_file_path)
+  name_chunks(temp_file_path, prefix = "my-prefix")
+  lines <- readLines(temp_file_path)
+  chunk_info <- get_chunk_info(lines)
+  expect_true(all(grepl("my-prefix", chunk_info$name[-1])))
+  file.remove(temp_file_path)
+})
+
 test_that("unnaming is advised when needed", {
   temp_file_path <- file.path(tmp, "example4.Rmd")
 
